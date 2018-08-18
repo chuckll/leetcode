7850a4c3964d3d5c619054ae989ab4e0983cccf9
chuckll <361818710@qq.com>
2018-08-18 17:01:21 +0800

    8/18 2

---
+++ array/array/RemoveDuplicatesfromSortedArray.cpp
@@ -0,0 +1,28 @@
+#include<vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.size() > 0)
+        {
+        int result = 1;
+        for(int i = 1; i < nums.size(); i++)
+        {
+            
+         if(nums[i] == nums[result-1] ) 
+             continue;
+            else{
+                nums[result] = nums[i];
+                result++;
+            }
+        }
+        return result;
+        }
+        else{
+            return 0;
+        }
+    }
+};
