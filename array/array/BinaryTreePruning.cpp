01baac5f2abf7e96cbbf889e3db9d3f58be2bb2e
chuckll <361818710@qq.com>
2018-11-15 20:44:40 +0800

    11/15

---
+++ array/array/BinaryTreePruning.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+using namespace std;
+
+struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ };
+
+
+class Solution {
+public:
+    TreeNode* pruneTree(TreeNode* root) {
+		if(root == NULL)
+			return NULL;
+		else{
+			if(root->val == 0 && root->left == NULL && root->right == NULL)
+				 return NULL;
+			else{
+				root->left = pruneTree(root->left);
+				root->right = pruneTree(root->right);
+				return root;
+			}
+		}
+    }
+};
