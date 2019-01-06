4b5cfb22912463727c21f85fa95bc3cb76b37f3f
chuckll <361818710@qq.com>
2019-01-06 19:59:05 +0800

    1/6

--- array/array/109-ConvertSortedListtoBinarySearchTree.cpp
+++ array/array/109-ConvertSortedListtoBinarySearchTree.cpp
@@ -18,38 +18,54 @@ using namespace std;
 class Solution {
 public:
 
-	TreeNode* GenerateTree(int left,int right,vector<int> s)
-	{
-			TreeNode* r;
-			int mid = (left + right)/2;
-			r->val = s[mid];
-			if(left <= mid-1)
-			{
-			    r->left = GenerateTree(left,mid-1,s);
-			}else
-			{
-				return r;
-			}
-			if(mid+1 <= right)
-			{
-			    r->right = GenerateTree(mid+1,right,s);
-			}else
-			{
-				return r;
-			}
-			return r;
+	//超出内存限制
+	//TreeNode* GenerateTree(TreeNode* root,int left,int right,vector<int> s)
+	//{
+	//	if(left <= right)
+	//	{
+	//		int mid = (left + right)  / 2;
+	//		root = new TreeNode(s[mid]);
+	//		root->left = GenerateTree(root->left,left,mid-1,s);
+	//		root->right = GenerateTree(root->right,mid+1,right,s);
+	//	}
+	//	return root;
+	//}
+
+
+
+  //  TreeNode* sortedListToBST(ListNode* head) {
+  //      vector<int> sort;
+		//while(head != NULL)
+		//{
+		//	sort.push_back(head->val);
+		//	head = head->next;
+		//}
+		//TreeNode* root = NULL;
+		//return GenerateTree(root,0,sort.size()-1,sort);
+  //  }
+
+	TreeNode* sortedListToBST(ListNode* head) {
+           //利用fast 和 slow 指针找到链表中点
+        return build(head,NULL);
 	}
 
-
-
-    TreeNode* sortedListToBST(ListNode* head) {
-        vector<int> sort;
-		while(head != NULL)
+	TreeNode* build(ListNode* start,ListNode* end)
+	{
+		if(start == end)
 		{
-			sort.push_back(head->val);
-			head = head->next;
+			return NULL;
 		}
-		TreeNode* root = GenerateTree(0,sort.size()-1,sort);
-		return root;
-    }
+		ListNode* fast = start;
+		ListNode* slow = start;
+		while( fast != end && fast->next != end)
+		{
+			fast = fast->next->next;
+			slow = slow->next;
+		}
+		TreeNode* node = new TreeNode(slow->val);
+		node->left = build(start,slow);
+		node->right = build(slow->next,end);
+		return node;
+	}
+
 };
