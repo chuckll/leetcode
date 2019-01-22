256d68a2ba2c0693d94b6db133a3f744223f7488
chuckll <361818710@qq.com>
2019-01-22 14:02:49 +0800

    1/22

--- array/array/232-ImplementQueueusingStacks.cpp
+++ array/array/232-ImplementQueueusingStacks.cpp
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stack>
+#include<vector>
 using namespace std;
 
 
@@ -12,17 +13,30 @@ public:
     
     /** Push element x to the back of queue. */
     void push(int x) {
+		vector<int> f;
+		while(!s.empty())
+		{
+			int num = s.top();
+			s.pop();
+			f.push_back(num);
+		}
 		s.push(x);
+		for(int i = f.size() - 1; i >= 0; i--)
+		{
+			s.push(f[i]);
+		}
     }
     
     /** Removes the element from in front of queue and returns that element. */
     int pop() {
-        
+        int num = s.top();
+		s.pop();
+		return num;
     }
     
     /** Get the front element. */
     int peek() {
-       
+       return s.top();
     }
     
     /** Returns whether the queue is empty. */
