3af70f589257d68c1e0b6244ea879b8a9672277a
chuckll <361818710@qq.com>
2019-03-01 19:18:03 +0800

    3/1

--- array/array/824-GoatLatin.cpp
+++ array/array/824-GoatLatin.cpp
@@ -24,15 +24,37 @@ public:
 			}
 		}
 		vstr.push_back(s);
+		bool isYuan = false;
 		for(int i = 0; i < vstr.size(); i++)
 		{
-			for(int j = 0; j < vstr[i].size(); j++)
+			if((vstr[i][0] == 'a' || vstr[i][0] == 'e' || vstr[i][0] == 'i' || vstr[i][0] == 'o' || vstr[i][0] == 'u' || vstr[i][0] == 'A' || vstr[i][0] == 'E' || vstr[i][0] == 'I' || vstr[i][0] == 'O' || vstr[i][0] == 'U'))
 			{
-				if()
-
-
+				isYuan = true;
+			}else{
+				isYuan = false;
 			}
-
+			string s1;
+			if(isYuan == false)
+			{
+				s1 = vstr[i].substr(1);
+				s1 += vstr[i][0];
+			}else{
+				s1 = vstr[i];
+			}
+			s1 += "ma";
+			for(int j = 0; j <= i; j++)
+			{
+				s1 += 'a';
+			}
+			result.push_back(s1);
 		}
+
+		for(int i = 0; i < result.size(); i++)
+		{
+			res.append(result[i]);
+		    if( i != result.size()-1)
+			     res += ' ';
+		}
+		return res;
     }
 };
--- array/array/884-UncommonWordsfromTwoSentences.cpp
+++ array/array/884-UncommonWordsfromTwoSentences.cpp
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<string>
 #include<vector>
+#include<map>
 using namespace std;
 
 
@@ -8,6 +9,57 @@ using namespace std;
 class Solution {
 public:
     vector<string> uncommonFromSentences(string A, string B) {
-        
+        map<string,int> smp;
+		vector<string> vs,res;
+		string s;
+		for(int i = 0; i < A.size(); i++)
+		{
+			if(A[i] == ' ')
+			{
+				vs.push_back(s);
+				s.clear();
+			}else{
+				s += A[i];
+			}
+		}
+		vs.push_back(s);
+		s.clear();
+		for(int i = 0; i < B.size(); i++)
+		{
+			if(B[i] == ' ')
+			{
+				vs.push_back(s);
+				s.clear();
+			}else{
+				s += B[i];
+			}
+		}
+		vs.push_back(s);
+
+		map<string,int>::iterator iter;
+		for(int i = 0; i < vs.size(); i++)
+		{
+			if(i == 0)
+				smp.insert(pair<string,int>(vs[i],1));
+			else{
+				iter = smp.find(vs[i]);
+				if( iter ==  smp.end())
+				{
+					smp.insert(pair<string,int>(vs[i],1));
+				}else
+				{
+					iter->second++;
+				}
+			}
+		}
+
+		for(iter = smp.begin(); iter != smp.end(); iter++)
+		{
+			if(iter->second == 1)
+			{
+				res.push_back(iter->first);
+			}
+		}
+		return res;
     }
 };
