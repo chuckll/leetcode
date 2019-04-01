624555d5df72ed5d788d51fcdaee93d9cfce47f3
chuckll <361818710@qq.com>
2019-04-01 20:04:54 +0800

    4/1

---
+++ array/array/100-SameTree.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<queue>
+using namespace std;
+
+
+
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ };
+
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+       if(p == NULL && q == NULL)
+		   return true;
+	   if(p != NULL && q != NULL && p->val == q->val)
+		   return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+	   else
+		   return false;
+
+	}
+};
--- array/array/784-LetterCasePermutation.cpp
+++ array/array/784-LetterCasePermutation.cpp
@@ -8,7 +8,30 @@ class Solution {
 public:
     vector<string> letterCasePermutation(string S) {
         vector<string> ans;
-		ans.push_back(S);
-
+	    DFS(ans,S,0);
+		return ans;
     }
+
+
+	void DFS(vector<string> &ans,string S,int i)
+	{
+		if(i == S.size())
+		{
+			ans.push_back(S);
+			return;
+		}else{
+			DFS(ans,S,i+1);
+			if(S[i] >= 'a' && S[i] <= 'z')
+			{
+				S[i] += 'A' - 'a';
+				DFS(ans,S,i+1);
+				S[i] -= 'A' - 'a';
+			}else if(S[i] >= 'A' && S[i] <= 'Z')
+			{
+				S[i] -= 'A' - 'a';
+				DFS(ans,S,i+1);
+				S[i] += 'A' - 'a';
+			}
+		}
+	}
 };
