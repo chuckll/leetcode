fe66b19c4876aab23cf37cc23b37b5f20563eb4c
chuckll <361818710@qq.com>
2019-05-05 19:50:24 +0800

    5/5

--- array/array/12-IntegertoRoman.cpp
+++ array/array/12-IntegertoRoman.cpp
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<string>
+#include<stack>
 using namespace std;
 
 
@@ -8,6 +9,17 @@ using namespace std;
 class Solution {
 public:
     string intToRoman(int num) {
-        
+        string res;
+		int values[13] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+		string keys[13] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+		for(int i = 0; i < 13; i++)
+		{
+			while(num >= values[i])
+			{
+				res.append(keys[i]);
+				num -= values[i];
+			}
+		}
+		return res;
     }
 };
--- array/array/148-SortList.cpp
+++ array/array/148-SortList.cpp
@@ -8,9 +8,49 @@ struct ListNode {
     ListNode(int x) : val(x), next(NULL) {}
 };
 
+
+
+//递归排序三部曲：1，快慢指针找中点；2，递归调用mergeSort，3，合并两个链表
 class Solution {
 public:
     ListNode* sortList(ListNode* head) {
-        
+        return mergeSort(head);
     }
+
+	ListNode* mergeSort(ListNode* node)
+	{
+		if(!node || !node->next) return node;
+		ListNode* fast = node;
+		ListNode* slow = node;
+		ListNode* breakN = node;
+		while(fast && fast->next)
+		{
+			fast = fast->next->next;
+			breakN = slow;
+			slow = slow->next;
+		}
+		breakN->next = nullptr;
+		ListNode* l = mergeSort(node);
+		ListNode* r = mergeSort(slow);
+		return merge(l,r);
+	}
+
+
+	ListNode* merge(ListNode* l,ListNode* r)
+	{
+		if(l == nullptr)
+			return r;
+		if(r == nullptr)
+			return l;
+		if(l->val < r->val)
+		{
+			l->next = merge(l->next,r);
+			return l;
+		}else{
+
+			r->next = merge(l,r->next);
+			return r;
+		}
+
+	}
 };
